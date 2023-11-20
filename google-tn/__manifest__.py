7289d693a003f5cd95734d340ffbe28e922379ca
odooTN <151531194+odooTN@users.noreply.github.com>
2023-11-20 18:26:31 +0530

    Add files via upload

---
+++ google-tn/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Google Looker Studio Connector",
+
+    'summary': """
+       	 Techneith makes the process of Odoo Google Looker Studio data integration easy, smooth, fast and reliable. Big Query big looker google looker connector google connector""",
+
+    'description': """
+    Techneith makes the process of Odoo Google Looker Studio data integration easy, smooth, fast and reliable. Big Query big looker google looker connector google connector """,
+         'live_test_url': 'https://techneith.com/products/odoo-looker-studio/',
+    'maintainer': 'TN Canada',
+    'author': "TN Canada",
+    'website': "https://techneith.com/products/odoo-looker-studio/",
+    'support': "homerobotic21@gmail.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '5.9',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/looker-odoo.jpg'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
